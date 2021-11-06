68ee8d5d34a07d2dfa5fe46f825dfcde958f9406
Omprakash Sridharan <omprakashsridharan@outlook.com>
2021-11-06 23:00:06 +0100

    Adds egui client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,28 @@
+use eframe::{egui, epi, run_native, NativeOptions};
+
+#[derive(Default)]
+pub struct TodoApp {}
+
 fn main() {
-    println!("Hello, world!");
+    let app = TodoApp::default();
+    let native_options = NativeOptions::default();
+    run_native(Box::new(app), native_options);
+}
+
+impl epi::App for TodoApp {
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        frame.quit();
+                    }
+                })
+            })
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Todo gRPC"
+    }
 }
